9285d452afe4b2ff0545483623af439371f2008e
NIMW <tridan243@gmail.com>
2024-09-03 01:11:18 +0700

    push code

---
+++ variable.dart
@@ -0,0 +1,24 @@
+// var name = 'Smith';
+// String name = 'Smith';
+// int num = 10;
+
+void  main() {
+  // String name = 1;
+
+  int num;
+  // print(num);
+
+  dynamic x = 'tom';
+  print(x);
+
+  final val1 = 12;
+  print(val1);
+
+  const pi = 3.14;
+  const area = pi*12*12;
+  print("The output is ${area}");
+
+  final v1 = 12;
+  const v2 = 13;
+  // v2 = 12
+}
